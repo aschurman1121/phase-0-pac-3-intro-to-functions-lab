de0f65d96b82bde96b54a4e74c9d03d80abc6944
alexandra <alexandraschurman@gmail.com>
2021-11-07 12:23:37 -0800

    commit tree f7928b347431f8d1a0189f7e68b46c8056655b2a

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-function shout(string) {
+global.expect = require("expect");
+
+const babel = require("babel-core");
+const jsdom = require("jsdom");
+const path = require("path");
+
+function shout(string){
     return string.toUpperCase();
 }
+console.log(shout("hello"));
+
 function whisper(string) {
-    return string.toLowerCase();
+    return string.toLowerCase()
 }
+console.log(whisper("HELLO"));
 
 function logShout(string){
     console.log(string.toUpperCase());
 }
+logShout("hello")
+
 
 function logWhisper(string){
     console.log(string.toLowerCase());
+
 }
+logWhisper("GOODBYE");
+
 
 function sayHiToGrandma(string) {
-    if (string === string.toLowerCase()) {
-        return "I can't hear you!"}
-     else if (string === string.toUpperCase()){
-        return "YES INDEED!"}
-     else if (string === "I love you, Grandma."){
-        return "I love you, too."}
+    if (string === string.toLowerCase());{
+        return ("I can't hear you!")
+    }
+    else if (string === string.toUpperCase()) {
+        return ("YES INDEED!")
+    }
+    } else if (string === 'I love you, Grandma.') {
+        return ("I love you, too.")
+    }
 }
